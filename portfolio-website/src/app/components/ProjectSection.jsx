0efb9f3f0dc909167ff1105f89877ec240ea1112
bry-ho <bryanho67@gmail.com>
2024-08-10 00:25:27 +0700

    feat: add ProjectSection.jsx

---
+++ portfolio-website/src/app/components/ProjectSection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProjectSection = () => {
+  return (
+    <div>
+  <h2 className="mt-10 text-center text-4xl font-bold text-white mb-4">
+    My Project / Experience
+  </h2>
+  <div className="flex justify-center">
+    <ul className="list-disc list-inside text-white lg:text-lg text-justify mt-3 mb-10 max-w-lg">
+      <li>Tugas Besar - IF1210 Dasar Pemrograman 2024</li>
+      <li>Divisi acara unit KMB (Keluarga Mahasiswa Buddhis)</li>
+      <li>Panitia Aku Masuk ITB (AMI) divisi MSDM</li>
+      <li>Panitia PPAB KMB 2024 divisi acara</li>
+    </ul>
+  </div>
+</div>
+  );
+};
+
+export default ProjectSection;
